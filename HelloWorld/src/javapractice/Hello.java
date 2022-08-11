5e8455b8f871ec3bbd1b5f1801d1d813e72ba17b
s8189882 <s8189882@gmail.com>
2022-08-11 15:27:49 +0530

    UC1 - Printing Hello World message; Using eclipse shortcuts

--- HelloWorld/src/javapractice/Hello.java
+++ HelloWorld/src/javapractice/Hello.java
@@ -1,7 +1,10 @@
 package javapractice;
 
 public class Hello {
+	
 	public static void main(String[] args) {
+		
+		//displaying Hello World message
 		System.out.println("Hello world!");
 		System.out.println("Welcome to Java");
 	}
